bbd82c39e1feced32d533b367ed7131eb07062a3
unknown <james@sctmes.com>
2024-05-02 12:17:26 +0800

    replace bash files at CRLF

--- bash/docker_entrypoint.sh
+++ bash/docker_entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -euo pipefail
-
 source auto_xvfb
-
 # 判断目标文件夹是否已经包含了指定的文件
 if ! diff -q /wine32/drive_c/Program\ Files/Common\ Files/Microsoft\ Shared/Speech/TTS/v11.0/ /wine32/drive_c/libs/chsbrkr_chtbrkr_dlls/ >/dev/null; then
     # 如果目标文件夹不包含指定的文件，则执行安装步骤
@@ -13,7 +11,6 @@ if ! diff -q /wine32/drive_c/Program\ Files/Common\ Files/Microsoft\ Shared/Spee
     wine msiexec /qn /i /wine32/drive_c/libs/language_packs_for_v11/MSSpeech_TTS_zh-HK_HunYee.msi
     cp /wine32/drive_c/libs/chsbrkr_chtbrkr_dlls/* /wine32/drive_c/Program\ Files/Common\ Files/Microsoft\ Shared/Speech/TTS/v11.0/
 fi
-
 # 运行应用程序
 cd /wine32/drive_c/bin
 wine MSSpeechServer.exe
