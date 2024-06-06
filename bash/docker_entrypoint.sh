49b4a204357ba54a67febab2f60155578d35dcd4
JamesChan <38025067+xeden3@users.noreply.github.com>
2024-06-06 16:35:44 +0800

    Update docker_entrypoint.sh
    Add RU KO JP lang packages AND NaturalLanguage6.dll

--- bash/docker_entrypoint.sh
+++ bash/docker_entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euo pipefail
+
 source auto_xvfb
+
 # 判断目标文件夹是否已经包含了指定的文件
 if ! diff -q /wine32/drive_c/Program\ Files/Common\ Files/Microsoft\ Shared/Speech/TTS/v11.0/ /wine32/drive_c/libs/chsbrkr_chtbrkr_dlls/ >/dev/null; then
     # 如果目标文件夹不包含指定的文件，则执行安装步骤
@@ -9,8 +11,13 @@ if ! diff -q /wine32/drive_c/Program\ Files/Common\ Files/Microsoft\ Shared/Spee
     wine msiexec /qn /i /wine32/drive_c/libs/language_packs_for_v11/MSSpeech_TTS_en-US_ZiraPro.msi
     wine msiexec /qn /i /wine32/drive_c/libs/language_packs_for_v11/MSSpeech_TTS_zh-CN_HuiHui.msi
     wine msiexec /qn /i /wine32/drive_c/libs/language_packs_for_v11/MSSpeech_TTS_zh-HK_HunYee.msi
+    wine msiexec /qn /i /wine32/drive_c/libs/language_packs_for_v11/MSSpeech_TTS_ja-JP_Haruka.msi
+    wine msiexec /qn /i /wine32/drive_c/libs/language_packs_for_v11/MSSpeech_TTS_ko-KR_Heami.msi
+    wine msiexec /qn /i /wine32/drive_c/libs/language_packs_for_v11/MSSpeech_TTS_ru-RU_Elena.msi
     cp /wine32/drive_c/libs/chsbrkr_chtbrkr_dlls/* /wine32/drive_c/Program\ Files/Common\ Files/Microsoft\ Shared/Speech/TTS/v11.0/
+    cp /wine32/drive_c/libs/chsbrkr_chtbrkr_dlls/NaturalLanguage6.dll /wine32/drive_c/windows/system32/
 fi
+
 # 运行应用程序
 cd /wine32/drive_c/bin
 wine MSSpeechServer.exe
